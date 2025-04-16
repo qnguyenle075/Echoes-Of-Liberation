97018d8a461daab3547168d008f0b2a50803db1a
qnguyenle075 <qnguyenle075@gmail.com>
2025-04-16 15:00:35 +0700

    them hieu ung no cho no chat

--- scene/explosion.gd
+++ scene/explosion.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 @onready var anim = $AnimatedSprite2D
+@onready var timer = $Timer
 
 func play_animation(name: String):
-	anim.animation = name
-	anim.play()
-	await anim.animation_finished
+	anim.play(name)
+
+func _ready():
+	timer.timeout.connect(_on_timeout)
+
+func _on_timeout():
 	queue_free()
